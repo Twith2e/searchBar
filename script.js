5c091829768674da5b929b6e6a92a55b64e0924a
twith2e <abdulbasitadebajo1619@gmail.com>
2024-06-27 08:00:41 +0100

    first deploy

---
+++ script.js
@@ -0,0 +1,61 @@
+let dataBase = [
+  "BANANA",
+  "APPLE",
+  "MANGO",
+  "STRAWBERRY",
+  "PEACH",
+  "PEAR",
+  "GRAPE",
+  "ORANGE",
+  "WATERMELON",
+  "NIGERIA",
+  "AUSTRALIA",
+  "DENMARK",
+  "FINLAND",
+  "GREECE",
+  "THAILAND",
+  "CHINA",
+  "RUSSIA",
+  "GHANA",
+  "EGYPT",
+  "KENYA",
+  "SOUTHAFRICA",
+];
+
+function showDb(array) {
+  return array.map((db) => {
+    result.innerHTML += `<li>${db.toLowerCase()}</li>`;
+  });
+}
+
+function showEntireList() {
+  result.style.display = "block";
+  showDb(dataBase);
+}
+
+function showList() {
+  result.style.display = "block";
+  result.innerHTML = "";
+  if (input.value) {
+    let filteredDb = dataBase.filter((db) => {
+      return db.includes(input.value.toUpperCase());
+    });
+    filteredDb.map((item) => {
+      const highlightedItem = item.replace(
+        new RegExp(input.value, "gi"),
+        (match) => {
+          return `<span class="highlight">${match}</span>`;
+        }
+      );
+      //   result.innerHTML += `<li>${match.toLowerCase()}</li>`;
+      console.log(highlightedItem);
+
+      const itemDiv = document.createElement("div");
+      itemDiv.innerHTML = highlightedItem.toLowerCase();
+      result.appendChild(itemDiv);
+    });
+  } else {
+    result.innerHTML = "";
+    showDb(dataBase);
+  }
+}
